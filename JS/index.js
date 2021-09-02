c75c816136f0e8abad47056ab3b0980453d4b96a
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-09-02 01:09:57 +0100

    Adding dark and light mode

--- JS/index.js
+++ JS/index.js
@@ -43,8 +43,40 @@ Response object of country
         pt: "Afeganistão
 */
 
+const countryComponent = {
+    props: ['country'],
+    template: `
+    <div class="country-element-container">
+        <img :src="country.flag" :alt="country.name + 'flag'" class="country-flag-image">
 
+        <div class="country-info-container">
+            <h3 class="u-heading-3 country-name">{{ country.name }}</h3>
 
+            <ul class="country-small-info-list">
+                <li class="country-small-info-list--item">
+                    <span class="country-small-info-list--item-property">Population:</span>
+                    {{ convertNumberToReadbleFormat(country.population) }}
+                </li>
+
+                <li class="country-small-info-list--item">
+                    <span class="country-small-info-list--item-property">Region:</span>
+                    {{ country.region }}
+                </li>
+
+                <li class="country-small-info-list--item">
+                    <span class="country-small-info-list--item-property">Capital:</span>
+                    {{ country.capital }}
+                </li>
+            </ul>
+        </div>
+    </div>
+   `,
+   methods: {
+        convertNumberToReadbleFormat: function (number) {
+            return number.toLocaleString('pt-br', { minimumFractionDigits: 2 })
+        }
+   }
+}
 
 const app = new Vue({
     el: '#app',
@@ -59,24 +91,24 @@ const app = new Vue({
         },
         getAllCountries: function () {
             fetch(BASE_API_URL + API_VERSION_AND_TYPE + API_END_POINT_ALL_COUNTRIES + "?" + FIELD_TO_ASK_IN_REQUEST)
-            .then((res) => res.json())
-            .then((data) => {
-                this.countries = data; 
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-        },
-        searchCountryByName: function() {
-            if (this.countryToSearch.trim() != '') {
-                fetch(BASE_API_URL + API_VERSION_AND_TYPE + API_END_POINT_SEARCH_BY_NAME + '/' + this.countryToSearch)
                 .then((res) => res.json())
                 .then((data) => {
-                    this.countries = data; 
+                    this.countries = data;
                 })
                 .catch((err) => {
                     console.log(err);
                 })
+        },
+        searchCountryByName: function () {
+            if (this.countryToSearch != '') {
+                fetch(BASE_API_URL + API_VERSION_AND_TYPE + API_END_POINT_SEARCH_BY_NAME + '/' + this.countryToSearch)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        this.countries = data;
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
             } else {
                 this.getAllCountries();
             }
@@ -84,17 +116,20 @@ const app = new Vue({
         searchCountryByRegion: function () {
             if (this.selectedRegion.toLowerCase() != 'all') {
                 fetch(BASE_API_URL + API_VERSION_AND_TYPE + API_END_POINT_SEARCH_BY_REGION + '/' + this.selectedRegion + "?" + FIELD_TO_ASK_IN_REQUEST)
-                .then((res) => res.json())
-                .then((data) => {
-                    this.countries = data; 
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        this.countries = data;
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
             } else {
                 this.getAllCountries();
             }
         }
+    },
+    components: {
+        'country-container': countryComponent
     }
 })
 
@@ -103,3 +138,11 @@ app.getAllCountries();
 
 
 
+
+let darkModeOn = false;
+
+document.querySelector('.dark-ligth-mode-button').addEventListener('click', () => {
+    document.querySelectorAll('link')[0].href = !darkModeOn ? './CSS/dark-variables.css' : './CSS/ligth-variables.css';
+    darkModeOn = !darkModeOn;
+    document.querySelector('.dark-ligth-mode-button').innerText = !darkModeOn ? 'Dark Mode' : 'Light Mode';
+})
